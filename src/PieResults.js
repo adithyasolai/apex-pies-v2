1104173c08372ae6cdcf057f32b8821d070f0683
Adithya Solai <adithyasolai7@gmail.com>
2024-06-22 18:27:38 -0700

    removed unneeded useStates

--- src/PieResults.js
+++ src/PieResults.js
@@ -7,18 +7,21 @@ import Plot from 'react-plotly.js';
 
 const PieResults = (props) => {
   const { currentUser } = useAuth();
-  const [uid, setUid] = useState(props.location.state.uid);
-  const [age, setAge] = useState(props.location.state.age);
-  const [risk, setRisk] = useState(props.location.state.risk);
-  const [sector, setSector] = useState(props.location.state.sector);
+  const uid = useRef(props.location.state.uid);
+  const age = useRef(props.location.state.age);
+  const risk = useRef(props.location.state.risk);
+  const sector = useRef(props.location.state.sector);
 
   const [loading, setLoading] = useState(true);
-  const [plotConfig, setPlotConfig] = useState({});
-  const [numStocks, setNumStocks] = useState();
 
+  // holds backend response data
   const pie = useRef(null);
   const pieRows = useRef(null);
   const avgBeta = useRef(0);
+
+  // data calculated when backend data is received to make render logic faster
+  const plotConfig = useRef(null)
+  const numStocks = useRef(0)
 
   // local dev endpoint
   const flask_endpoint = "http://127.0.0.1:5000/fetchpies"
@@ -37,7 +40,7 @@ const PieResults = (props) => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            uid: uid,
+            uid: uid.current,
             is_guest: currentUser ? false : true
           }),
         });
@@ -46,27 +49,17 @@ const PieResults = (props) => {
         const json = await response.json();
 
         // Put all the results from the backend server into our State to be rendered.
-        // Remove the loading screen so that the page can finally be rendered.
-        setLoading(false);
         pie.current = json.pie
         pieRows.current = json.pieRows
         avgBeta.current = (Math.round(json.avgBeta * 100) / 100).toFixed(2)
-        setNumStocks(json.pie['Beta'].length); // just using any of the lists to get the length
-
-        console.log(json.pieRows)
-        console.log(pie)
-        console.log(pieRows)
-
-        // extract just the data needed for hovertext display,
-        // and put it into the 2D array format needed for the plot
-        var pieRowsOfRowsAsCustomData = pieRows.current.map((dict) => [dict['Name'], dict['Sector'], dict['Market Cap'], dict['Beta']])
+        numStocks.current = json.pie['Beta'].length // just using any of the lists to get the length
 
         // construct plot configs as soon as results from backend come
         var data = [{
           values: pie.current['Percentage'],
           labels: pie.current['Ticker'],
           type: 'pie',
-          customdata: pieRowsOfRowsAsCustomData, 
+          customdata: pieRows.current.map((dict) => [dict['Name'], dict['Sector'], dict['Market Cap'], dict['Beta']]), 
           hovertemplate: 'Ticker: %{label} <br> Name: %{customdata[0][0]} <br> Sector: %{customdata[0][1]} <br> Market Cap: $%{customdata[0][2]} M <br> Beta: %{customdata[0][3]}<extra></extra>',
           marker: {
             colors: pie.current['Color']
@@ -79,10 +72,13 @@ const PieResults = (props) => {
           showlegend: false
         }
 
-        setPlotConfig({
+        plotConfig.current = {
           'data': data,
           'layout': layout
-        })
+        }
+
+        // Remove the loading screen so that the page can finally be rendered.
+        setLoading(false);
 
       } catch (err) {
         console.log(err);
@@ -104,11 +100,11 @@ const PieResults = (props) => {
     <div className="text-center bg-primary" style={{paddingTop: "75px"}}>
       {/* Display fields chosen by user in User Form */}
       <h1>
-        Age: {age}
+        Age: {age.current}
         <br />
-        Risk: {risk}
+        Risk: {risk.current}
         <br />
-        Sector: {sector}
+        Sector: {sector.current}
       </h1>
 
       {/* Beta */}
@@ -116,12 +112,12 @@ const PieResults = (props) => {
       <h3> Overall Beta of Pie: {avgBeta.current} </h3>
 
       <Plot
-        data={plotConfig['data']}
-        layout={plotConfig['layout']}
+        data={plotConfig.current['data']}
+        layout={plotConfig.current['layout']}
       />
 
       {/* Lists information about each stock in our Pie */}
-      {Array.from(Array(numStocks), (x, i) => i).map((stockIndex) => {
+      {Array.from(Array(numStocks.current), (x, i) => i).map((stockIndex) => {
           return (
             <p key={stockIndex}>
               Percentage: {pie.current["Percentage"][stockIndex]}% Sector: {pie.current["Sector"][stockIndex]} Ticker: {pie.current["Ticker"][stockIndex]}
