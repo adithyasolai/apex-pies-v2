029a4d70dab3d1510a7abbe35ee3efe184e05be7
Adithya Solai <adithyasolai7@gmail.com>
2024-06-24 21:46:24 -0700

    make resourcesfaq text follow style of website

--- src/ResourcesFaq.js
+++ src/ResourcesFaq.js
@@ -23,14 +23,15 @@ const ResourcesFaq = () => {
         triggerClassName="collapsible"
         triggerOpenedClassName="collapsible"
       >
-        <p>
-          {" "}
-          Diversification is a technique that reduces risk by allocating
-          investments among various financial instruments, industries and
-          other categories. It aims to maximize return by investing in
-          different areas that should each react differently to changes in
-          market conditions.
-        </p>
+        <div style={{ maxWidth: "50%", width: "50%", marginLeft: "25%" }}>
+          <p className="display-4 fs-2 text-black" style={{ width: "100%" }}>
+            Diversification is a technique that reduces risk by allocating
+            investments among various financial instruments, industries and
+            other categories. It aims to maximize return by investing in
+            different areas that should each react differently to changes in
+            market conditions.
+          </p>
+        </div>
       </Collapsible>
 
       <br />
@@ -48,15 +49,16 @@ const ResourcesFaq = () => {
         triggerClassName="collapsible"
         triggerOpenedClassName="collapsible"
       >
-        <p>
-          {" "}
-          A stock is considered risky when it does not have a lot of earnings
-          history. This is important because it proves to investors that the
-          company has a history of generating cash for the business. Another
-          aspect that makes a stock risky is the lack of time on the market.
-          If the stock has recently gone public, it will be more volatile due
-          to investors not knowing how the stock should be valued.
-        </p>
+        <div style={{ maxWidth: "50%", width: "50%", marginLeft: "25%" }}>
+          <p className="display-4 fs-2 text-black" style={{ width: "100%" }}>
+            A stock is considered risky when it does not have a lot of earnings
+            history. This is important because it proves to investors that the
+            company has a history of generating cash for the business. Another
+            aspect that makes a stock risky is the lack of time on the market.
+            If the stock has recently gone public, it will be more volatile due
+            to investors not knowing how the stock should be valued.
+          </p>
+        </div>
       </Collapsible>
 
       <br />
@@ -74,36 +76,14 @@ const ResourcesFaq = () => {
         triggerClassName="collapsible"
         triggerOpenedClassName="collapsible"
       >
-        <p>
-          {" "}
-          The Standard and Poor's 500, or simply the S&P 500, is a stock
-          market index tracking the performance of 500 large companies listed
-          on stock exchanges in the United States. It is one of the most
-          commonly followed equity indices
-        </p>
-      </Collapsible>
-
-      <br />
-
-      <Collapsible
-        trigger={
-          <Button 
-            type="Submit" 
-            variant="secondary" 
-            size="lg"
-          >
-            Resources
-          </Button>
-        }
-        triggerClassName="collapsible"
-        triggerOpenedClassName="collapsible"
-      >
-        <p>
-          {" "}
-          https://www.youtube.com/watch?v=bJHr6_skXWc <br />{" "}
-          https://www.youtube.com/watch?v=Pmlsa5-ruMk
-          <br /> https://www.youtube.com/watch?v=4gn4F1VmTvM{" "}
-        </p>
+        <div style={{ maxWidth: "50%", width: "50%", marginLeft: "25%" }}>
+          <p className="display-4 fs-2 text-black" style={{ width: "100%" }}>
+            The Standard and Poor's 500, or simply the S&P 500, is a stock
+            market index tracking the performance of 500 large companies listed
+            on stock exchanges in the United States. It is one of the most
+            commonly followed equity indices.
+          </p>
+        </div>
       </Collapsible>
     </div>
   );
