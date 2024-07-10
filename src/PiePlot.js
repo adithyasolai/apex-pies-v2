05bb83e0e693526646043d9ddcc28bb0294d8d24
Adithya Solai <adithyasolai7@gmail.com>
2024-07-10 00:11:59 -0700

    simplify pie to only have sectors as slices

--- src/PiePlot.js
+++ src/PiePlot.js
@@ -57,22 +57,58 @@ const PiePlot = (props) => {
       pie.current = json.pie
       pieRows.current = json.pieRows
 
+      // simplify pie chart with just sector slices only
+      var sector_data_dict = {}
+      pieRows.current.forEach(row => {
+        var currSector = row["Sector"]
+        var currPct = row["Percentage"]
+        var currColor = row["Color"]
+
+        if (currSector in sector_data_dict) {
+          sector_data_dict[currSector][0] += currPct
+        } else {
+          sector_data_dict[currSector] = new Array(2)
+          sector_data_dict[currSector][0] = currPct
+          sector_data_dict[currSector][1] = currColor
+        }
+      });
+      
+      // create exact ordering of Sector keys
+      var sector_list = Object.keys(sector_data_dict)
+      var colors = new Array(sector_list.length)
+      var percentages = new Array(sector_list.length)
+
+      sector_list.forEach((sector, index) => {
+        percentages[index] = sector_data_dict[sector][0]
+        colors[index] = sector_data_dict[sector][1]
+      });
       // construct plot configs as soon as results from backend come
       var data = [{
-        values: pie.current['Percentage'],
-        labels: pie.current['Ticker'],
+        values: percentages,
+        labels: sector_list,
         type: 'pie',
-        customdata: pieRows.current.map((dict) => [dict['Name'], dict['Sector'], dict['Market Cap'], dict['Beta']]), 
-        hovertemplate: 'Ticker: %{label} <br> Name: %{customdata[0][0]} <br> Sector: %{customdata[0][1]} <br> Market Cap: $%{customdata[0][2]} M <br> Beta: %{customdata[0][3]}<extra></extra>',
+        // customdata: pieRows.current.map((dict) => [dict['Name'], dict['Sector'], dict['Market Cap'], dict['Beta']]), 
+        // hovertemplate: 'Ticker: %{label} <br> Name: %{customdata[0][0]} <br> Sector: %{customdata[0][1]} <br> Market Cap: $%{customdata[0][2]} M <br> Beta: %{customdata[0][3]}<extra></extra>',
         marker: {
-          colors: pie.current['Color']
-        }
+          colors: colors
+        },
+        hoverinfo: 'none'
       }]
+      // var data = [{
+      //   values: pie.current['Percentage'],
+      //   labels: pie.current['Ticker'],
+      //   type: 'pie',
+      //   customdata: pieRows.current.map((dict) => [dict['Name'], dict['Sector'], dict['Market Cap'], dict['Beta']]), 
+      //   hovertemplate: 'Ticker: %{label} <br> Name: %{customdata[0][0]} <br> Sector: %{customdata[0][1]} <br> Market Cap: $%{customdata[0][2]} M <br> Beta: %{customdata[0][3]}<extra></extra>',
+      //   marker: {
+      //     colors: pie.current['Color']
+      //   }
+      // }]
 
       var layout = {
         paper_bgcolor: 'rgba(0,0,0,0)', 
-        plot_bgcolor: 'rgba(0,0,0,0)',
-        showlegend: false
+        plot_bgcolor: 'rgba(0,0,0,0)'
+        // showlegend: false
       }
 
       plotConfig.current = {
