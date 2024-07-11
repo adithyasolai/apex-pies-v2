5db2d5ec00b748e1c86c77e5e53024711e1f5bf5
Adithya Solai <adithyasolai7@gmail.com>
2024-07-10 21:49:18 -0700

    make Resources/FAQ responsive, reduce font size, and use Bootstrap components

--- src/ResourcesFaq.js
+++ src/ResourcesFaq.js
@@ -1,90 +1,89 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import Collapsible from "react-collapsible";
 
 const ResourcesFaq = () => {
   return (
     <Container fluid className="bg-primary vh-100 text-center navbar-padding-top-extra">
-      <h1> Resources/FAQs</h1>
+      <Row className="bg-primary">
+        <Col/>
+        <Col xs={12} md={6}>
+          <h1 className="pb-2"> Resources/FAQs</h1>
 
-      <br />
-
-      <br />
-
-      <Collapsible
-        trigger={
-          <Button 
-            type="Submit" 
-            variant="secondary" 
-            size="lg"
+          <Collapsible
+            trigger={
+              <Button 
+                type="Submit" 
+                variant="secondary" 
+                size="lg"
+              >
+                Why is diversification important?
+              </Button>
+            }
+            triggerClassName="collapsible"
+            triggerOpenedClassName="collapsible"
           >
-            Why is diversification important?
-          </Button>
-        }
-        triggerClassName="collapsible"
-        triggerOpenedClassName="collapsible"
-      >
-        <div style={{ maxWidth: "50%", width: "50%", marginLeft: "25%" }}>
-          <p className="display-6 fs-2 text-black" style={{ width: "100%" }}>
-            Diversification is a technique that reduces risk by allocating
-            investments among various financial instruments, industries and
-            other categories. It aims to maximize return by investing in
-            different areas that should each react differently to changes in
-            market conditions.
-          </p>
-        </div>
-      </Collapsible>
+            <p className="display-6 fs-5 pt-2">
+              Diversification is a technique that reduces risk by allocating
+              investments among various financial instruments, industries and
+              other categories. It aims to maximize return by investing in
+              different areas that should each react differently to changes in
+              market conditions.
+            </p>
+          </Collapsible>
 
-      <br />
+          <br/>
 
-      <Collapsible
-        trigger={
-          <Button 
-            type="Submit" 
-            variant="secondary" 
-            size="lg"
+          <Collapsible
+            trigger={
+              <Button 
+                type="Submit" 
+                variant="secondary" 
+                size="lg"
+              >
+                What makes a stock risky?
+              </Button>
+            }
+            triggerClassName="collapsible"
+            triggerOpenedClassName="collapsible"
           >
-            What makes a stock risky?
-          </Button>
-        }
-        triggerClassName="collapsible"
-        triggerOpenedClassName="collapsible"
-      >
-        <div style={{ maxWidth: "50%", width: "50%", marginLeft: "25%" }}>
-          <p className="display-6 fs-2 text-black" style={{ width: "100%" }}>
-            A stock is considered risky when it does not have a lot of earnings
-            history. This is important because it proves to investors that the
-            company has a history of generating cash for the business. Another
-            aspect that makes a stock risky is the lack of time on the market.
-            If the stock has recently gone public, it will be more volatile due
-            to investors not knowing how the stock should be valued.
-          </p>
-        </div>
-      </Collapsible>
+            <p className="display-6 fs-5 pt-2">
+              A stock is considered risky when it does not have a lot of earnings
+              history. This is important because it proves to investors that the
+              company has a history of generating cash for the business. Another
+              aspect that makes a stock risky is the lack of time on the market.
+              If the stock has recently gone public, it will be more volatile due
+              to investors not knowing how the stock should be valued.
+            </p>
+          </Collapsible>
 
-      <br />
+          <br/>
 
-      <Collapsible
-        trigger={
-          <Button 
-            type="Submit" 
-            variant="secondary" 
-            size="lg"
+          <Collapsible
+            trigger={
+              <Button 
+                type="Submit" 
+                variant="secondary" 
+                size="lg"
+              >
+                What is the S&P 500?
+              </Button>
+            }
+            triggerClassName="collapsible"
+            triggerOpenedClassName="collapsible"
           >
-            What is the S&P 500?
-          </Button>
-        }
-        triggerClassName="collapsible"
-        triggerOpenedClassName="collapsible"
-      >
-        <div style={{ maxWidth: "50%", width: "50%", marginLeft: "25%" }}>
-          <p className="display-6 fs-2 text-black" style={{ width: "100%" }}>
-            The Standard and Poor's 500, or simply the S&P 500, is a stock
-            market index tracking the performance of 500 large companies listed
-            on stock exchanges in the United States. It is one of the most
-            commonly followed equity indices.
-          </p>
-        </div>
-      </Collapsible>
+            <p className="display-6 fs-5 pt-2">
+              The Standard and Poor's 500, or simply the S&P 500, is a stock
+              market index tracking the performance of 500 large companies listed
+              on stock exchanges in the United States. It is one of the most
+              commonly followed equity indices.
+            </p>
+          </Collapsible>
+        
+        </Col>
+        <Col/>
+
+
+      </Row>
     </Container>
   );
 }
