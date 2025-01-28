4592c85ed244c6c58d81f406b211c9f8b9c8204a
Adithya Solai <adithyasolai7@gmail.com>
2025-01-28 00:12:37 -0500

    migrate ApexHover component to tsx

---
+++ src/VisualComponents/ApexHover.tsx
@@ -0,0 +1,26 @@
+import React, { JSX } from "react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Placement } from "react-bootstrap/esm/types";
+import { useMediaQuery } from "react-responsive";
+
+interface ApexHoverProps {
+  hoverText: string;
+  children: React.ReactElement;
+}
+
+export const ApexHover: React.FC<ApexHoverProps> = ({ hoverText, children }): JSX.Element => {
+  // Determine if the current screen size is 'xs' or 'md'
+  // TODO: Consider doing this globally somewhere instead of each time this component is used/rendered.
+  const isXsScreen: boolean = useMediaQuery({ maxWidth: 400 });
+  const hoverTextPlacement: Placement = isXsScreen ? "bottom" : "right";
+
+  return (
+    <OverlayTrigger
+      placement={hoverTextPlacement}
+      overlay={<Tooltip>{hoverText}</Tooltip>}
+      trigger={["focus", "hover"]}
+    >
+      {children}
+    </OverlayTrigger>
+  );
+};
