b25a24cf1d619aaf79f216713fe9acc430b89b84
Adithya Solai <adithyasolai7@gmail.com>
2025-01-30 00:28:57 -0500

    fix user login check by de-composing auth context user hook value, like before

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -9,7 +9,7 @@ interface PrivateRouteProps {
 // Makes sure that the page can only be accessed if we are currently logged into a user
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   // TODO: After migrating AuthContext.js to tsx, add typing to this `currentUser` variable.
-  const currentUser = useAuth();
+  const { currentUser } = useAuth();
 
   return currentUser ? children : <Navigate to="/login" replace />;
 };
