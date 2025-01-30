ca9c327f864b61fb49005b1840c097fb7aac1eea
Adithya Solai <adithyasolai7@gmail.com>
2025-01-29 21:07:34 -0500

    migrate Resources/FAQ page to tsx, and use more modular components

---
+++ src/ResourcesFaq.tsx
@@ -0,0 +1,55 @@
+import React, { JSX } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Collapsible from "react-collapsible";
+
+import faqPairsJson from "./resources/faqPairs.json"
+
+interface FaqPair {
+  question: string,
+  answer: string
+}
+
+const FaqItem: React.FC<FaqPair> = ({question, answer}) => (
+  <Collapsible
+    trigger={
+      <Button type="submit" variant="secondary" size="lg">
+        {question}
+      </Button>
+    }
+    triggerClassName="collapsible"
+    triggerOpenedClassName="collapsible"
+  >
+    <p className="display-6 fs-5 pt-2">
+      {answer}
+    </p>
+  </Collapsible>
+);
+
+const ResourcesFaq: React.FC = (): JSX.Element => {
+  // import question & answer text from JSON file.
+  const faqPairs: FaqPair[] = faqPairsJson['FAQ Pairs']
+
+  return (
+    <Container
+      fluid
+      className="bg-primary vh-100 text-center navbar-padding-top-extra"
+    >
+      <Row className="bg-primary">
+        <Col />
+        <Col xs={12} md={6}>
+          <h1 className="pb-2"> Resources/FAQs</h1>
+
+          {faqPairs.map((faqPair: FaqPair, index: number) => (
+            <React.Fragment key={index}>
+              <FaqItem {...faqPair}/>
+              {index < faqPairs.length - 1 && <br />}
+            </React.Fragment>
+          ))}
+        </Col>
+        <Col />
+      </Row>
+    </Container>
+  );
+};
+
+export default ResourcesFaq;
