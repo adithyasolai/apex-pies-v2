92943f74eaff907b3b94118b0533767693adbf51
Adithya Solai <adithyasolai7@gmail.com>
2025-02-08 21:07:05 -0500

    create local pies carousel component in MyPies page to improve readability

--- src/MyPies.tsx
+++ src/MyPies.tsx
@@ -5,6 +5,48 @@ import { ApexMyPiesLogicalFields, useApexMyPies } from "./useApexMyPies";
 import { ApexPieTable } from "./VisualComponents/ApexPieTable";
 import { ApexPieInputDisplay } from "./VisualComponents/ApexPieInputDisplay";
 
+interface ApexPiesCarouselProps {
+  numSaved: number;
+  activePie: number;
+  handleSelect: (selectedIndex: number, e: any) => void;
+}
+
+const ApexPiesCarousel: React.FC<ApexPiesCarouselProps> = ({numSaved, activePie, handleSelect}) => {
+  return (
+    <Carousel
+      activeIndex={activePie}
+      onSelect={handleSelect}
+      data-bs-theme="dark" // makes left/arrows black
+      interval={null} // disables auto-play of carousel
+      controls={true} // making left/right arrows show up
+      fade={false} // use this to toggle slide vs fade animation while testing
+    >
+      {Array.from(Array(Math.min(numSaved, 4)), (x, i) => i).map(
+        (i) => {
+          return (
+            <Carousel.Item key={i}>
+              <Container fluid>
+                <Row>
+                  <Col />
+                  <Col xs={12} md={8}>
+                    {/* The `numSaved-i` allows the most recent 4 pies to be shown */}
+                    {/* It works because the PieNums in the backend start at 1, not 0. */}
+                    <PiePlot
+                      pieNum={numSaved - i}
+                      active={activePie === i}
+                    />
+                  </Col>
+                  <Col />
+                </Row>
+              </Container>
+            </Carousel.Item>
+          );
+        }
+      )}
+    </Carousel>
+  )
+}
+
 export const MyPies = () => {
   const {
     numSaved,
@@ -52,37 +94,7 @@ export const MyPies = () => {
               {numSaved === 1 ? (
                 <PiePlot pieNum={numSaved} active={true} />
               ) : (
-                <Carousel
-                  activeIndex={activePie}
-                  onSelect={handleSelect}
-                  data-bs-theme="dark" // makes left/arrows black
-                  interval={null} // disables auto-play of carousel
-                  controls={true} // making left/right arrows show up
-                  fade={false} // use this to toggle slide vs fade animation while testing
-                >
-                  {Array.from(Array(Math.min(numSaved, 4)), (x, i) => i).map(
-                    (i) => {
-                      return (
-                        <Carousel.Item key={i}>
-                          <Container fluid>
-                            <Row>
-                              <Col />
-                              <Col xs={12} md={8}>
-                                {/* The `numSaved-i` allows the most recent 4 pies to be shown */}
-                                {/* It works because the PieNums in the backend start at 1, not 0. */}
-                                <PiePlot
-                                  pieNum={numSaved - i}
-                                  active={activePie === i}
-                                />
-                              </Col>
-                              <Col />
-                            </Row>
-                          </Container>
-                        </Carousel.Item>
-                      );
-                    }
-                  )}
-                </Carousel>
+                <ApexPiesCarousel numSaved={numSaved} activePie={activePie} handleSelect={handleSelect} />
               )}
             </Col>
             <Col />
