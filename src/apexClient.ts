7ec03f648f4ad72fe03d4e7f8c804e1562ceac29
Adithya Solai <adithyasolai7@gmail.com>
2025-02-09 11:52:16 -0500

    implement generic Apex backend call to further reduce repeated code

--- src/apexClient.ts
+++ src/apexClient.ts
@@ -7,47 +7,43 @@ const apiEndpoints: ApexApiEndpoints = process.env.REACT_APP_DEV_MODE
   ? apiEndpointsDev
   : apiEndpointsProd;
 
-const fetchSavedPieEndpoint: string = apiEndpoints["fetchSavedPieEndpoint"];
+type supportedHttpMethodTypes = "GET" | "POST"
 
+interface genericApexCallInput {
+  endpoint: string
+  httpMethodType: supportedHttpMethodTypes
+  inputParams: any
+}
+
+// Send request to backend server using given endpoint and input params,
+// and return the response.
+export const genericApexCall = async({endpoint, httpMethodType, inputParams}: genericApexCallInput): Promise<any> => {
+  const response = await fetch(endpoint, {
+    method: httpMethodType,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(inputParams),
+  });
+
+  return await response.json();
+}
+
+// Fetch pie data for given Pie from give account
+const fetchSavedPieEndpoint: string = apiEndpoints["fetchSavedPieEndpoint"];
 export interface fetchSavedPieDataInput {
   uid: string;
   pieNum: number
 }
-
 export const fetchSavedPieData = async ({uid, pieNum}: fetchSavedPieDataInput): Promise<any> => {
-  // Send request to backend server to fetch the Pie & Plotly information
-  // for the current userId. Wait for the request to give a response.
-  const response = await fetch(fetchSavedPieEndpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      uid: uid,
-      pieNum: pieNum.toString(),
-    }),
-  });
-
-  return await response.json();
-}
-
-const fetchNumSavedEndpoint = apiEndpoints["fetchNumSavedEndpoint"];
-
-export interface fetchNumSavedPiesInput {
-  uid: string;
+  return await genericApexCall({endpoint: fetchSavedPieEndpoint, httpMethodType: "POST", inputParams: {uid: uid, pieNum: pieNum.toString()}})
 }
 
 // Fetch number of Pies saved to the given account
+const fetchNumSavedEndpoint = apiEndpoints["fetchNumSavedEndpoint"];
+export interface fetchNumSavedPiesInput {
+  uid: string;
+}
 export const fetchNumSavedPies = async ({uid}: fetchNumSavedPiesInput): Promise<any> => {
-  const response = await fetch(fetchNumSavedEndpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      uid: uid
-    }),
-  });
-
-  return await response.json();
+  return await genericApexCall({endpoint: fetchNumSavedEndpoint, httpMethodType: "POST", inputParams: {uid: uid}})
 }
